3590376f5463b9c46a7bad60581317da71614783
Андрей Тарасов <tarasov.krk@mail.ru>
2022-03-30 10:46:15 +0700

    final

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,23 @@
 $(document).ready(() => {
   $('#profile-slider').slick({
     dots: true,
-    arrows:false,
+    arrows: false,
     infinite: true,
     speed: 2000,
     slidesToShow: 1,
-    autoplay: true
-  });
-  $('.product-slider__item').show();
-  $('.profile-slider__preloader').hide();
+    autoplay: true,
+  })
+  $('.product-slider__item').show()
+  $('.profile-slider__preloader').hide()
+  $('.window_popup-close').click(function () {
+    $(this).closest('.window_popup').hide()
+  })
+  $('#open_form_add_year').click(function (e) {
+    e.preventDefault()
+    $('#add-year').show()
+  })
+  $('#open_form_add_event').click(function (e) {
+    e.preventDefault()
+    $('#add-event').show()
+  })
 })
