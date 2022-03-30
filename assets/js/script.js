43a1b45fc959d5eea109deb921b40eac913dfc29
Андрей Тарасов <tarasov.krk@mail.ru>
2022-03-30 22:28:45 +0700

    final

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,4 +20,23 @@ $(document).ready(() => {
     e.preventDefault()
     $('#add-event').show()
   })
+  $('.photo-hover-layout__item').mouseenter(function () {
+    $(this).append('<div id=\'people_name\'>' + $(this).data('name') + '</div>')
+  }).mouseleave(function () {
+    $('#people_name').remove()
+  }).click(function () {
+    let name = $(this).data('name')
+    let img = $(this).data('img')
+    let desc = $(this).data('desc')
+    let window = $('#popup-photo')
+
+    window.find('.pupup-full_name').text(name)
+    if (!img) {
+      window.find('.pupup-photo').hide()
+    } else {
+      window.find('.pupup-photo').show().attr('src', img)
+    }
+    window.find('.pupup-desc').text(desc)
+    window.show()
+  })
 })
