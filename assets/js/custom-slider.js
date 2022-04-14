b0699a03fcf5d702a3286cfde9af77b8320fc4c7
Андрей Тарасов <tarasov.krk@mail.ru>
2022-04-14 11:41:57 +0700

    final

--- assets/js/custom-slider.js
+++ assets/js/custom-slider.js
@@ -1,9 +1,22 @@
 $(document).ready(() => {
   let odd = 2
+  let baseLeft = parseInt($('.custom-slider .container').css('left'))
 
   $('.ui-card:nth-child(' + odd + ')').addClass('active')
   $('.ui-card:nth-child(' + odd + ')').prev().addClass('prev')
   $('.ui-card:nth-child(' + odd + ')').next().addClass('next')
+
+  window.addEventListener("orientationchange", event => {
+    $('.custom-slider .container').removeAttr("style")
+    setTimeout(()=>{
+      baseLeft = parseInt($('.custom-slider .container').css('left'))
+      $('.custom-slider .container').css({ left: baseLeft})
+      $('.custom-slider .container > div').removeClass('prev active next')
+      $('.custom-slider .container > div:nth-child(1)').addClass('prev')
+      $('.custom-slider .container > div:nth-child(2)').addClass('active')
+      $('.custom-slider .container > div:nth-child(3)').addClass('next')
+    }, 100)
+  });
 
   $('.ui-card').click(function () {
     let slide = $('.active').width()
@@ -21,7 +34,7 @@ $(document).ready(() => {
 
     if ($(this).hasClass('item-last')) {
       $('.ui-card').hide()
-      $('.container').stop(false, true).css({ left: 484 })
+      $('.container').stop(false, true).css({ left: baseLeft })
       $('.ui-card').show()
       first.addClass('active')
       first.prev().addClass('prev')
@@ -29,7 +42,7 @@ $(document).ready(() => {
     }
     else if($(this).hasClass('item-zero')) {
       $('.ui-card').hide()
-      $('.container').stop(false, true).css({ left: -484 })
+      $('.container').stop(false, true).css({ left: -baseLeft })
       $('.ui-card').show()
       prelast.addClass('active')
       prelast.prev().addClass('prev')
